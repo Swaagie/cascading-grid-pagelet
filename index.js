507a01fedbec2996cda28b4cf461dca2a05db505
Swaagie <info@martijnswaagman.nl>
2014-08-18 17:24:29 +0200

    [minor] initial visual work on blocks

--- index.js
+++ index.js
@@ -1,5 +1,50 @@
 'use strict';
 
 require('pagelet').extend({
+  view: 'view.hbs',
+  css: 'css.styl',
 
+  //
+  // Dimensions of the block container, used to calculate the
+  // relative width and height in percentages.
+  //
+  width: '100%',
+  height: '600px',
+
+  //
+  // Collection of objects, each object represents one block. Blocks have a
+  // default width and height of (25/3)%. meaning the
+  //
+  blocks: [{
+    title: 'Web frameworks',
+    sub: 'Popular Node.JS frameworks',
+    link: '/explore/frameworks',
+    height: 4,
+    width: 7,
+    hover: [
+      {
+        caption: 'modules',
+        icon: 'file',
+        n: 12
+      },
+      {
+        caption: 'contributors',
+        icon: 'users',
+        n: 20
+      }
+    ]
+  }],
+
+  //
+  // Called on GET.
+  //
+  get: function get(render) {
+    render(null, {
+      blocks: this.blocks,
+      dimension: {
+        height: this.height,
+        width: this.width
+      }
+    })
+  }
 }).on(module);
